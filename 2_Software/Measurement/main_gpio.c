dbc34a381db39d5668fc00de0d64ea40f9a5e9af
Nathan Loretan <nathan.loretan@nordicsemi.no>
2017-08-02 15:22:29 +0200

    GPIO set time test

--- 2_Software/Measurement/main_gpio.c
+++ 2_Software/Measurement/main_gpio.c
@@ -2,8 +2,22 @@
 #include "../nordic_components/components/device/nrf52840_bitfields.h"
 
 #define PORT 	NRF_P0
-#define PIN1 	0x2000
-#define PIN2 	0x4000
+#define PIN1 	13
+#define PIN2 	14
+
+#define DEBUG_PIN_SET(_pin, _port, _flag)             \
+do {                                                  \
+  if(_flag)                                           \
+    _port->OUTSET = (0x01 << _pin);                   \
+  else                                                \
+    _port->OUTCLR = (0x01 << _pin);                   \
+} while(0)
+
+#define DEBUG_PIN_TOGGLE(_pin, _port)                                           \
+do {                                                                            \
+    (_port->OUT & (0x01 << _pin)) ?   (_port->OUTCLR = (0x01 << _pin)) :        \
+                                      (_port->OUTSET = (0x01 << _pin));         \
+} while(0)
 
 int main()
 {
@@ -14,11 +28,16 @@ int main()
 	// NRF_P0->OUTCLR = 0x2000;
 	// NRF_P0->OUTCLR = 0x4000;
 
-  // while(1)
-  // {
+	DEBUG_PIN_SET(PIN1, PORT, 0);
+	DEBUG_PIN_SET(PIN2, PORT, 0);
+
+  while(1)
+  {
 	// 	NRF_P0->OUTSET = 0x2000;
+	DEBUG_PIN_SET(PIN1, PORT, 1);
+	DEBUG_PIN_SET(PIN2, PORT, 1);
 	// 	NRF_P0->OUTSET = 0x4000;
-  // }
+  }
 
   return 0;
 }
